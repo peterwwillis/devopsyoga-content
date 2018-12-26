c67fd24a3461b983ab05a28784472f7d284ef11a
Peter Willis <peterwwillis@gmail.com>
2018-12-26 17:10:14 -0500

    Fix calling script from different directories

--- tools/tool_chest/call-py.sh
+++ tools/tool_chest/call-py.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
-MYDIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+MYFILE=$(readlink -f ${BASH_SOURCE[0]})
+MYDIR=$(dirname ${MYFILE})
 MDBN=`basename $MYDIR`
-source $MYDIR/$MDBN.venv/bin/activate
+ACT="$MYDIR/$MDBN.venv/bin/activate"
+if [ ! -r "$ACT" ] ; then
+    echo "Error: could not load '$ACT'" ; exit 1
+fi
+source $ACT
 BN=`basename $0 .sh`
 ./$BN.py $*
